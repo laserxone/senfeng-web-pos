f9fa5fe9faaf9bed1105639c92b7fe9b7631697c
ubaid1840 <107393880+ubaid1840@users.noreply.github.com>
2025-03-28 07:07:31 +0500

    testing

--- src/lib/firebaseAdmin.js
+++ src/lib/firebaseAdmin.js
@@ -1,8 +1,9 @@
 import admin from 'firebase-admin';
 
-const serviceAccountBase64 = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64;
-const serviceAccount = JSON.parse(Buffer.from(serviceAccountBase64, 'base64').toString('utf-8'));
+// const serviceAccountBase64 = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64;
+// const serviceAccount = JSON.parse(Buffer.from(serviceAccountBase64, 'base64').toString('utf-8'));
 
+const serviceAccount = {}
 
 if (!admin.apps.length) {
     admin.initializeApp({
