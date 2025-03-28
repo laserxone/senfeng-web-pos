2d90253f67b05ab612f30416073a4f5a2e42a71d
ubaid1840 <107393880+ubaid1840@users.noreply.github.com>
2025-03-28 07:26:09 +0500

    converted to base64

--- src/lib/firebaseAdmin.js
+++ src/lib/firebaseAdmin.js
@@ -1,9 +1,8 @@
 import admin from 'firebase-admin';
 
-// const serviceAccountBase64 = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64;
-// const serviceAccount = JSON.parse(Buffer.from(serviceAccountBase64, 'base64').toString('utf-8'));
+const serviceAccountBase64 = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64;
+const serviceAccount = JSON.parse(Buffer.from(serviceAccountBase64, 'base64').toString('utf-8'));
 
-const serviceAccount = {}
 
 if (!admin.apps.length) {
     admin.initializeApp({
