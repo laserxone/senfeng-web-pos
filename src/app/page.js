28426115cc8556940487241c2caff0cae6d2f7c4
ubaid1840 <ubaidrehman1840@gmail.com>
2025-05-09 14:24:20 +0500

    site stopped

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,10 @@ import POS from "@/components/POS";
 
 export default function Home() {
  return (
-    <POS />
+   //  <POS />
+
+   <div style={{width:'100vw', height:'100vh', display:'flex', alignItems:'center', justifyContent:'center'}}>
+      <p>Site not accessible</p>
+   </div>
  )
 }
